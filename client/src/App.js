a60442cd1e65c3416d97d1692060ae62ee74a78c
ismael <ismaelrr888@gmail.com>
2019-12-16 15:12:17 -0500

    fix run server + ui + add page

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import NavBar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>My React App</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
