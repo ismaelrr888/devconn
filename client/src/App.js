425b60a01a0afa3feb08e1e2a8f96288b49eb4d4
ismael <ismaelrr888@gmail.com>
2019-12-16 15:37:59 -0500

    fix router register + log in

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import NavBar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 
 import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Landing />
-
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
