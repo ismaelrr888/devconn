744af9a2cd8a6f963bfb58a6c4823e87248af6a0
ismael <ismaelrr888@gmail.com>
2020-01-04 23:56:13 -0500

    fix add comment to post + clear errors

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import * as types from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors);
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -103,8 +104,27 @@ export const removeLike = id => dispatch => {
 
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: types.GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: types.GET_POST,
@@ -123,5 +143,12 @@ export const addComment = (postId, commentData) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: types.POST_LOADING
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: types.CLEAR_ERRORS
   };
 };
--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -6,6 +6,8 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case types.GET_ERRORS:
       return action.payload;
+    case types.CLEAR_ERRORS:
+      return {};
     default:
       return state;
   }
